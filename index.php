f58d64c8834ea9fdcf9b435f5d6c38256d6a0dc0
sideman05 <146353124+sideman05@users.noreply.github.com>
2025-06-08 16:44:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if (!empty($_POST['task']) &&  isset($_POST['task'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TODO</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
